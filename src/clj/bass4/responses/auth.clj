230c4cf633824af9dd06522f7dcd8f277fd3cd4f
brjann <brjann@gmail.com>
2017-03-15 22:35:22 +0100

    Double auth mock seems to be working.

--- src/clj/bass4/responses/auth.clj
+++ src/clj/bass4/responses/auth.clj
@@ -5,19 +5,47 @@
             [ring.util.http-response :as response]
             [schema.core :as s]))
 
-;; Confused over whether much of this logic should be moved to the service
-(defn- double-auth-ok? [session]
+(defn- not-authenticated? [session]
+  (or (nil? (:identity session))
+      (nil? (user/get-user (:identity session)))))
+
+#_(defn- double-auth-ok? [session]
   (not (or (nil? (:identity session))
            (nil? (user/get-user (:identity session)))
            (not (auth-service/double-auth-required?))
            (nil? (:double-auth-code session)))))
 
-(defn double-auth-page [session]
-  (if (double-auth-ok? session)
-    (auth-view/double-auth (:double-auth-code session))
-    (response/unauthorized "You are not supposed to be here")))
+(defn- not-double-auth-ok? [session]
+  (or (not (auth-service/double-auth-required?))
+      (nil? (:double-auth-code session))))
 
+(defn- double-auth-done? [session]
+  (:double-authed session))
+
+(defn- double-auth-redirect [session]
+  (cond
+    (not-authenticated? session) "/login"
+    (double-auth-done? session) "/user/"
+    ;should this be more complex? double auth may be broken
+    (not-double-auth-ok? session) "/login")
+  )
+
+(defn double-auth-page [session]
+  (if-let [redirect (double-auth-redirect session)]
+    (response/found redirect)
+    (auth-view/double-auth (:double-auth-code session))))
+
+;; TODO: Add schema validation
 (defn double-auth-check [code session]
+  (if-let [redirect (double-auth-redirect session)]
+    (response/found redirect)
+    (if (= code (:double-auth-code session))
+      (-> (response/found "/user/")
+          (assoc :session (assoc session :double-authed 1 :double-auth-code nil)))
+      ;; TODO: Add error message to double auth
+      (response/found "/double-auth"))))
+
+#_(defn double-auth-check [code session]
   (if (double-auth-ok? session)
     (if (= code (:double-auth-code session))
       (-> (response/found "/user/")
