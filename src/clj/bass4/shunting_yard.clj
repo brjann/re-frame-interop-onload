8442e80efd122cdfe077f807fca12f86bca6f87d
brjann <brjann@gmail.com>
2017-05-13 20:18:30 +0200

    Infix parser now with namespace

--- src/clj/bass4/shunting_yard.clj
+++ src/clj/bass4/shunting_yard.clj
@@ -1,33 +1,17 @@
 (ns bass4.shunting-yard
   (:require [clojure.math.numeric-tower :as math]))
 
-(defn- to-chars
+#_(defn- to-chars
   [x]
   (clojure.string/split (clojure.string/replace x " " "") #""))
 
-(defn- digit?
-  [x]
-  (and x (re-find #"^\d+$" x)))
-
-(defn- double-op?
-  [op]
-  (some (partial = op) ["||" "&&" "==" ">=" "<=" "!="]))
 
 ;; Adapted from
 ;; http://eddmann.com/posts/infix-calculator-in-clojure/
 
-(defn tokenize [expr]
-  (reverse
-    (reduce
-      (fn [[t & ts :as tokens] token]
-        (cond
-          (and (digit? token) (digit? t)) (cons (str t token) ts)
-          (double-op? (str t token)) (cons (str t token) ts)
-          :else (cons token tokens)))
-      '(), (to-chars expr))))
-
-
-;; 	array('~', '^', '*', '/',  '-', '+', '==', '!=', '>', '<', '>=', '<=', '&&', '||', '(' );
+(defn- to-chars
+  [x]
+  (clojure.string/split x #""))
 
 ;; http://en.cppreference.com/w/c/language/operator_precedence
 (def ops {"||" 1,
@@ -38,6 +22,32 @@
           "*" 6, "/" 6,
           "^" 7,
           "~" 10})
+
+(def op-chars (concat (distinct (clojure.string/split (clojure.string/join (keys ops)) #"")) ["(" ")" " "]))
+
+(defn- has-op-char?
+  [x]
+  (some (partial clojure.string/includes? x) op-chars))
+
+(defn- digit?
+  [x]
+  (and x (re-find #"^\d+$" x)))
+
+(defn- double-op?
+  [op]
+  (some (partial = op) ["||" "&&" "==" ">=" "<=" "!="]))
+
+(defn tokenize [expr]
+  (filter
+    #(not= " " %)
+    (reverse
+            (reduce
+              (fn [[t & ts :as tokens] token]
+                (cond
+                  (not (has-op-char? (str t token))) (cons (str t token) ts)
+                  (double-op? (str t token)) (cons (str t token) ts)
+                  :else (cons token tokens)))
+              '(), (to-chars expr)))))
 
 (defn- shunting-yard [[rpn stack last-op?] token]
     (let [less-op? #(and (contains? ops %) (<= (ops token) (ops %)))
@@ -57,42 +67,52 @@
                [[] () true]
                tokens))))
 
-(def truthy? (partial not= 0))
+(defn token-resolver
+  ([] (token-resolver {}))
+  ([namespace] (token-resolver namespace nil))
+  ([namespace default]
+   (fn
+     [token]
+     (if (nil? (re-find #"^\d*\.?\d*$" token))
+       (if-let [res (or (namespace token)
+                        (when (= "$" (subs token 0 1))
+                          default))]
+         res
+         (throw (Exception. (str "Var " token " not found"))))
+       (read-string token)))))
 
-(defn bool01
-  [x]
-  (if x 1 0))
+(defn rpn
+  ([tokens] (rpn tokens (token-resolver)))
+  ([tokens token-resolver-fn]
+   (let [truthy? (partial not= 0)
+         bool01 (fn [x] (if x 1 0))
+         ops {"||" #(bool01 (or (truthy? %1) (truthy? %2)))
+              "&&" #(bool01 (and (truthy? %1) (truthy? %2)))
+              "==" #(bool01 (= %1 %2))
+              "!=" #(bool01 (not= %1 %2))
+              "<"  #(bool01 (< %1 %2))
+              ">"  #(bool01 (> %1 %2))
+              "<=" #(bool01 (<= %1 %2))
+              ">=" #(bool01 (>= %1 %2))
+              "+"  +
+              "-"  -
+              "*"  *
+              '"/" /
+              "^" math/expt
+              "~" -}]
+     (first
+       (reduce
+         (fn [stack token]
+           (if (contains? ops token)
+             (cons ((ops token) (second stack) (first stack)) (drop 2 stack))
+             (cons (token-resolver-fn token) stack)))
+         [] tokens)))))
 
-(defn rpn [tokens]
-  (let [ops {"||" #(bool01 (or (truthy? %1) (truthy? %2)))
-             "&&" #(bool01 (and (truthy? %1) (truthy? %2)))
-             "==" #(bool01 (= %1 %2))
-             "!=" #(bool01 (not= %1 %2))
-             "<"  #(bool01 (< %1 %2))
-             ">"  #(bool01 (> %1 %2))
-             "<=" #(bool01 (<= %1 %2))
-             ">=" #(bool01 (>= %1 %2))
-             "+"  +
-             "-"  -
-             "*"  *
-             '"/" /
-             "^" math/expt
-             "~" -}]
-    (first
-      (reduce
-        (fn [stack token]
-          (if (contains? ops token)
-            (cons ((ops token) (second stack) (first stack)) (drop 2 stack))
-            (cons (read-string token) stack)))
-        [] tokens))))
+(defn calc
+  ([expr] (calc expr {}))
+  ([expr namespace]
+   (-> expr
+       tokenize
+       parse-tokens
+       (rpn (token-resolver namespace)))))
 
-(def log #(do (println %) %))
-
-(def calc (comp rpn parse-tokens tokenize))
-
-(def calc-debug (comp rpn log parse-tokens log tokenize))
-;;(calc-debug "3 + 4 * 5 / (3 + 2)")
-
-(defn testx
-  [x [y z]]
-  (str x y z))
