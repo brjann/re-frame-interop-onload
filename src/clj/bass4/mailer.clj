743aba47e6def12f7210d19cdb2761a7731bbc9a
brjann <brjann@gmail.com>
2017-06-13 23:38:44 +0200

    Mailer added.

---
+++ src/clj/bass4/mailer.clj
@@ -0,0 +1,14 @@
+(ns bass4.mailer
+  (:require [bass4.config :refer [env]]
+            [clojure.java.io :as io]
+            [bass4.php_clj.core :refer [clj->php]]))
+
+(defn mail!
+  ([to subject message] (mail! to subject message nil))
+  ([to subject message reply-to]
+   (let [mailer-path (io/file (env :bass-path) "system/ExternalMailer.php")
+         args        (into {}
+                           (filter second
+                                   {"to" to "subject" subject "message" message "reply-to" reply-to}))]
+     (println (str mailer-path))
+     (clojure.java.shell/sh "php" (str mailer-path) :in (clj->php args)))))
