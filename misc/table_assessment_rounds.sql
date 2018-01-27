fbd6fe7e98cd2d7b84a926032d814ef090a28a6c
brjann <brjann@gmail.com>
2018-01-27 16:13:35 +0100

    Updated assessment rounds create statement to match db table.

--- misc/table_assessment_rounds.sql
+++ misc/table_assessment_rounds.sql
@@ -1,15 +1,15 @@
 CREATE TABLE `assessment_rounds` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `RoundId` int(11) DEFAULT NULL,
-  `Time` timestamp NULL DEFAULT NULL,
+  `Time` datetime DEFAULT NULL,
   `UserId` int(11) DEFAULT NULL,
   `BatchId` int(11) DEFAULT NULL,
   `Step` int(11) DEFAULT NULL,
   `InstrumentId` int(11) DEFAULT NULL,
   `Texts` text,
   `AdministrationId` int(11) DEFAULT NULL,
-  `Completed` timestamp NULL DEFAULT NULL,
+  `Completed` datetime DEFAULT NULL,
   `MustShowTexts` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE KEY `RoundIdStep` (`RoundId`,`Step`)
-)
+  KEY `RoundIdStep` (`RoundId`,`Step`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
