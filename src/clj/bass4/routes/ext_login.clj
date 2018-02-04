16ea3d78f39025bab30c40405676591a407ee319
brjann <brjann@gmail.com>
2018-02-04 12:42:43 +0100

    IP check and tests.

--- src/clj/bass4/routes/ext_login.clj
+++ src/clj/bass4/routes/ext_login.clj
@@ -8,7 +8,7 @@
             [ring.util.request :as request]
             [bass4.utils :refer [map-map str->int]]
             [bass4.config :refer [env]]
-            [bass4.captcha :as captcha]
+            [clojure.string :as string]
             [clojure.java.io :as io]
             [bass4.bass-locals :as locals]
             [bass4.responses.instrument :as instruments]
@@ -19,11 +19,18 @@
             [ring.util.codec :as codec]
             [clojure.tools.logging :as log]))
 
+(defn match-request-ip
+  [request ips-str]
+  (let [remote-ip   (:remote-addr request)
+        allowed-ips (into #{} (mapv #(first (string/split % #" ")) (string/split-lines ips-str)))]
+    (contains? allowed-ips remote-ip)))
+
 (defn check-ip
   [handler request]
   (let [{:keys [allowed ips]} (db/bool-cols db/ext-login-settings {} [:allowed])]
     (cond
       (not allowed) (layout/text-response "0 External login not allowed")
+      (not (match-request-ip request ips)) (layout/text-response (str "0 External login not allowed from this IP " (:remote-addr request)))
       :else (handler request))))
 
 (defroutes ext-login-routes
---
+++ test/clj/bass4/test/reqs_ext_login.clj
@@ -0,0 +1,33 @@
+(ns bass4.test.reqs-ext-login
+  (:require [bass4.i18n]
+            [clojure.test :refer :all]
+            [bass4.handler :refer :all]
+            [kerodon.core :refer :all]
+            [kerodon.test :refer :all]
+            [bass4.test.core :refer [test-fixtures not-text? log-return]]
+            [bass4.services.auth :as auth-service]
+            [bass4.db.core :as db]
+            [bass4.services.user :as user]
+            [clj-time.core :as t]))
+
+(use-fixtures
+  :once
+  test-fixtures)
+
+(deftest request-ext-login-not-allowed
+  (with-redefs [db/ext-login-settings (constantly {:allowed false :ips ""})]
+    (-> (session (app))
+        (visit "/ext-login/checkpending")
+        (has (some-text? "0 External login not allowed")))))
+
+(deftest request-ext-login-allowed-wrong-ip
+  (with-redefs [db/ext-login-settings (constantly {:allowed true :ips ""})]
+    (-> (session (app))
+        (visit "/ext-login/checkpending")
+        (has (some-text? "0 External login not allowed from this IP")))))
+
+(deftest request-ext-login-allowed-ok-ip
+  (with-redefs [db/ext-login-settings (constantly {:allowed true :ips "localhost"})]
+    (-> (session (app))
+        (visit "/ext-login/checkpending")
+        (has (some-text? "checkpending")))))
