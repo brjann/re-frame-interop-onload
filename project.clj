729bdb91ab28903966083f5e069001bcf2d6b398
brjann <brjann@gmail.com>
2018-07-31 19:43:35 +0200

    Sorted libraries. Upgraded test libraries. Removed humane-test-output.

--- project.clj
+++ project.clj
@@ -3,40 +3,52 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
 
-  :dependencies [[buddy/buddy-hashers "1.3.0"]              ; https://github.com/funcool/buddy-hashers
-                 [compojure "1.6.1"]                        ; https://github.com/weavejester/compojure
-                 [conman "0.8.2"]                           ; https://github.com/luminus-framework/conman
-                 [cprop "0.1.11"]                           ; https://github.com/tolitius/cprop
-                 [org.clojure/core.async "0.4.474"]         ; https://github.com/clojure/core.async
-                 [luminus-immutant "0.2.4"]                 ; https://github.com/luminus-framework/luminus-immutant
-                 [luminus-nrepl "0.1.4"]                    ; https://github.com/luminus-framework/luminus-nrepl
-                 [markdown-clj "1.0.2"]                     ; https://github.com/yogthos/markdown-clj
-                 [metosin/ring-http-response "0.9.0"]       ; https://github.com/metosin/ring-http-response
-                 [mount "0.1.12"]                           ; https://github.com/tolitius/mount
-                 [mysql/mysql-connector-java "8.0.11"]      ; https://mvnrepository.com/artifact/mysql/mysql-connector-java
+  :dependencies [
+                 ; Clojure libraries
                  [org.clojure/clojure "1.9.0"]              ; https://github.com/clojure/clojure
+                 [org.clojure/core.async "0.4.474"]         ; https://github.com/clojure/core.async
                  [org.clojure/tools.cli "0.3.7"]            ; https://github.com/clojure/tools.cli
                  [org.clojure/tools.logging "0.4.1"]        ; https://github.com/clojure/tools.logging
+                 [org.clojure/math.numeric-tower "0.0.4"]   ; https://github.com/clojure/math.numeric-tower
+                 [org.clojure/data.json "0.2.6"]            ; https://github.com/clojure/data.json
+
+                 ; Web server libraries
+                 [luminus-immutant "0.2.4"]                 ; https://github.com/luminus-framework/luminus-immutant
+                 [compojure "1.6.1"]                        ; https://github.com/weavejester/compojure
+                 [ring-middleware-format "0.7.2"]           ; https://github.com/ngrunwald/ring-middleware-format
+                 [ring-webjars "0.2.0"]                     ; https://github.com/weavejester/ring-webjars
+                 [ring/ring-core "1.7.0-RC1"]               ; https://github.com/ring-clojure/ring
+                 [ring/ring-defaults "0.3.2"]               ; https://github.com/ring-clojure/ring-defaults
+                 [prone "1.6.0"]                            ; https://github.com/magnars/prone
+                 [metosin/ring-http-response "0.9.0"]       ; https://github.com/metosin/ring-http-response
+                 [prismatic/schema "1.1.9"]                 ; https://github.com/plumatic/schema
+
+                 ; Database and state management libraries
+                 [conman "0.8.2"]                           ; https://github.com/luminus-framework/conman
+                 [mount "0.1.12"]                           ; https://github.com/tolitius/mount
+                 [mysql/mysql-connector-java "8.0.11"]      ; https://mvnrepository.com/artifact/mysql/mysql-connector-java
+                 [cprop "0.1.11"]                           ; https://github.com/tolitius/cprop
+                 [luminus-nrepl "0.1.4"]                    ; https://github.com/luminus-framework/luminus-nrepl
+
+                 ; HTML and language libraries
+                 [selmer "1.11.8"]                          ; https://github.com/yogthos/Selmer
+                 [markdown-clj "1.0.2"]                     ; https://github.com/yogthos/markdown-clj
+                 [com.taoensso/tempura "1.2.1"]             ; https://github.com/ptaoussanis/tempura
+
+                 ; Utility libraries
+                 [buddy/buddy-hashers "1.3.0"]              ; https://github.com/funcool/buddy-hashers
+                 [clj-http "3.9.0"]                         ; https://github.com/dakrone/clj-http
+                 [org.flatland/ordered "1.5.6"]             ; https://github.com/amalloy/ordered
+                 [clj-time "0.14.4"]                        ; https://github.com/clj-time/clj-time
+                 [camel-snake-kebab "0.4.0"]                ; https://github.com/qerub/camel-snake-kebab
+
+                 ; Webjars
                  [org.webjars/bootstrap "4.1.2"]            ; https://github.com/twbs/bootstrap
                  [org.webjars/font-awesome "4.7.0"]         ; https://fontawesome.com/
                  [org.webjars/jquery "3.3.1"]               ; https://jquery.com/
                  [org.webjars/jquery-ui "1.12.1"]           ; https://jqueryui.com/
                  [org.webjars.bower/jquery-color "2.1.2"]   ; https://github.com/jquery/jquery-color
-                 [org.webjars/webjars-locator-jboss-vfs "0.1.0"] ;
-                 [ring-middleware-format "0.7.2"]           ; https://github.com/ngrunwald/ring-middleware-format
-                 [ring-webjars "0.2.0"]                     ; https://github.com/weavejester/ring-webjars
-                 [ring/ring-core "1.7.0-RC1"]               ; https://github.com/ring-clojure/ring
-                 [ring/ring-defaults "0.3.2"]               ; https://github.com/ring-clojure/ring-defaults
-                 [selmer "1.11.8"]                          ; https://github.com/yogthos/Selmer
-                 [com.taoensso/tempura "1.2.1"]             ; https://github.com/ptaoussanis/tempura
-                 [clj-time "0.14.4"]                        ; https://github.com/clj-time/clj-time
-                 [prismatic/schema "1.1.9"]                 ; https://github.com/plumatic/schema
-                 [org.flatland/ordered "1.5.6"]             ; https://github.com/amalloy/ordered
-                 [org.clojure/math.numeric-tower "0.0.4"]   ; https://github.com/clojure/math.numeric-tower
-                 [org.clojure/data.json "0.2.6"]            ; https://github.com/clojure/data.json
-                 [prone "1.6.0"]                            ; https://github.com/magnars/prone
-                 [camel-snake-kebab "0.4.0"]                ; https://github.com/qerub/camel-snake-kebab
-                 [clj-http "3.9.0"]]                        ; https://github.com/dakrone/clj-http
+                 [org.webjars/webjars-locator-jboss-vfs "0.1.0"]]
 
   :min-lein-version "2.0.0"
 
@@ -51,33 +63,29 @@
 
   ;; Difference between plugins and dependencies:
   ;; https://www.quora.com/In-Clojure-whats-the-difference-between-plugins-dependencies-require-use-import-etc
-  :plugins [[lein-cprop "1.0.1"]
+  :plugins [[lein-cprop "1.0.3"]
             [lein-immutant "2.1.0"]]
 
   :profiles
-  {:uberjar {:omit-source true
-             :aot :all
-             :uberjar-name "bass4.jar"
-             :source-paths ["env/prod/clj"]
-             :resource-paths ["env/prod/resources"]}
+  {:uberjar       {:omit-source    true
+                   :aot            :all
+                   :uberjar-name   "bass4.jar"
+                   :source-paths   ["env/prod/clj"]
+                   :resource-paths ["env/prod/resources"]}
 
    :dev           [:project/dev :profiles/dev]
    :test          [:project/dev :project/test :profiles/test]
 
    ;; These dependencies are only compiled into the development app
-   :project/dev  {:dependencies [[ring/ring-mock "0.3.0"]
-                                 [ring/ring-devel "1.5.1"]
-                                 [pjstadig/humane-test-output "0.8.1"]
-                                 [peridot "0.4.4"]
-                                 [philoskim/debux "0.4.8"]
-                                 [kerodon "0.8.0"]]
-                  :plugins      [[com.jakemccrary/lein-test-refresh "0.20.0"]]
+   :project/dev   {:dependencies   [[ring/ring-devel "1.7.0-RC1"] ; Used for reloading namespaces before web requests
+                                    [peridot "0.5.1"]       ; https://github.com/xeqi/peridot
+                                    [kerodon "0.9.0"]       ; https://github.com/xeqi/kerodon
+                                    [philoskim/debux "0.4.8"]] ; https://github.com/philoskim/debux
+                   :plugins        [[com.jakemccrary/lein-test-refresh "0.20.0"]]
 
-                  :source-paths ["env/dev/clj"]
-                  :resource-paths ["env/dev/resources"]
-                  :repl-options {:init-ns user}
-                  :injections [(require 'pjstadig.humane-test-output)
-                               (pjstadig.humane-test-output/activate!)]}
-   :project/test {:resource-paths ["env/test/resources"]}
-   :profiles/dev {}
+                   :source-paths   ["env/dev/clj"]
+                   :resource-paths ["env/dev/resources"]
+                   :repl-options   {:init-ns user}}
+   :project/test  {:resource-paths ["env/test/resources"]}
+   :profiles/dev  {}
    :profiles/test {}})
