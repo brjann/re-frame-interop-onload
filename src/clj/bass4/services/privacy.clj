0675135399b21f23d5ca6723cf7f51a119745800
brjann <brjann@gmail.com>
2018-08-09 22:29:35 +0200

    Privacy notice service returns nil on empty notice.

--- src/clj/bass4/services/privacy.clj
+++ src/clj/bass4/services/privacy.clj
@@ -1,24 +1,25 @@
 (ns bass4.services.privacy
   (:require [bass4.db.core :as db]
-            [buddy.hashers :as hashers]
-            [bass4.config :as config]
             [clojure.string :as s]))
+
+(defn- trim-sql-notice
+  [privacy-notice]
+  (let [privacy-notice (some-> privacy-notice
+                               (:privacy-notice)
+                               (s/trim))]
+    (when-not (empty? privacy-notice)
+      privacy-notice)))
 
 (defn get-project-privacy-notice
   [project-id]
-  (some-> (db/get-project-privacy-notice {:project-id project-id})
-          (:privacy-notice)
-          (s/trim)))
+  (trim-sql-notice (db/get-project-privacy-notice {:project-id project-id})))
 
 (defn get-db-privacy-notice
   []
-  (some-> (db/get-db-privacy-notice)
-          (:privacy-notice)
-          (s/trim)))
+  (trim-sql-notice (db/get-db-privacy-notice)))
 
 (defn get-privacy-notice
-  [project-id]
-  (let [privacy-notice (get-project-privacy-notice project-id)]
-    (if (empty? privacy-notice)
-      (get-db-privacy-notice)
-      privacy-notice)))
+  ([] (get-privacy-notice nil))
+  ([project-id]
+   (or (get-project-privacy-notice project-id)
+       (get-db-privacy-notice))))
