a23e284e9aab2fb274467a66a4c5e717e072eedb
brjann <brjann@gmail.com>
2018-11-07 13:30:21 +0100

    Remove all old def-api functions. All tests passing.

--- src/clj/bass4/api_coercion.clj
+++ src/clj/bass4/api_coercion.clj
@@ -7,16 +7,14 @@
   (:import (clojure.lang Symbol)
            (java.net URL)))
 
-(def try-url (URL. "http://example.com"))
 
-(defn URL?
-  [s]
-  (let [test-url (try (URL. s)
-                      (catch Exception _
-                        (URL. try-url s)))]
-    (when (empty? (.getHost test-url))
-      (throw (Exception.))))
-  s)
+
+;; -----------------------------
+;;    VALIDATORS AND COERCERS
+;; -----------------------------
+
+
+(def try-url (URL. "http://example.com"))
 
 (defn url?
   [s]
@@ -30,10 +28,6 @@
         s)
       (catch Exception _))))
 
-(defn ?URL?
-  [s]
-  (when-not (nil? s) (URL? s)))
-
 (defn int!
   [i]
   (let [x (cond
@@ -44,8 +38,7 @@
             (read-string i)
 
             :else
-            nil
-            #_(throw (Exception.)))]
+            nil)]
     x))
 
 (defn bool!
@@ -55,32 +48,10 @@
     (let [x (int! x)]
       (not (zero? x)))))
 
-
-
-(defn JSON-map!
-  [s]
-  (let [m (json/read-str s)]
-    (when-not (map? m)
-      (throw (Exception.)))
-    m))
-
 (defn json!
   [s]
   (try (json/read-str s)
        (catch Exception _)))
-
-(defn ?str!
-  [s]
-  (if (nil? s)
-    nil
-    (str s)))
-
-(defn str+!
-  [s]
-  (let [s (str s)]
-    (if (zero? (count s))
-      (throw (Exception.))
-      s)))
 
 (defn str?
   [s min max]
@@ -88,34 +59,10 @@
     (let [l (count s)]
       (and (<= min l) (>= max l)))))
 
-(defn ?map?
-  [m]
-  (or (nil? m) (map? m)))
 
-(defn eval-spec
-  [api-name spec v spec-name v-name]
-  (let [api-exception (fn [msg]
-                        (throw (ex-info
-                                 (str "API validation failed. API: "
-                                      api-name
-                                      ". Spec-name: "
-                                      spec-name
-                                      ". Arg name: " v-name
-                                      ". Arg value: " v
-                                      (when-not (empty? msg)
-                                        (str ". Message: " msg)))
-                                 {:type ::api-exception})))
-        validation?   (= \? (last spec-name))
-        nil-ok?       (= \? (first spec-name))]
-    (try (let [ret (spec v)]
-           (if validation?
-             (if (or ret (and (nil? v) nil-ok?))
-               v
-               (api-exception "Validation failed"))
-             ret))
-         (catch Throwable e
-           (let [msg (.getMessage e)]
-             (api-exception msg))))))
+;; -------------------
+;;    DEFAPI MACRO
+;; -------------------
 
 (defn- extract-args
   [args]
@@ -133,32 +80,6 @@
                             [nil more])
         [arg-spec body] [(first more) (rest more)]]
     [doc-string arg-spec body]))
-
-(defmacro def-api
-  [api-name & more]
-  (when-not (instance? Symbol api-name)
-    (throw (IllegalArgumentException. "First argument to def-api must be a symbol")))
-  (let [[doc-string# arg-spec# body#] (destruct-api more)
-        arg-list#    (extract-args arg-spec#)
-        ns#          (ns-name *ns*)
-        speced-args# (filter #(= 2 (count %)) arg-list#)
-        let-vector#  (reduce #(concat %1 (list (first %2) `(eval-spec
-                                                             ~(str ns# "/" api-name)
-                                                             ~(second %2)
-                                                             ~(first %2)
-                                                             ~(name (second %2))
-                                                             ~(name (first %2)))))
-                             nil
-                             speced-args#)]
-    (concat
-      `(defn ~api-name)
-      (when doc-string#
-        (list doc-string#))
-      (list (into [] (map first arg-list#)))
-      (list (if (seq speced-args#)
-              `(let [~@let-vector#]
-                 ~@body#)
-              `(do ~@body#))))))
 
 
 (defn api-exception
