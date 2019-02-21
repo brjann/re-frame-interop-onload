129c57d82e94f75c40273db81e3b8385432c3689
brjann <brjann@gmail.com>
2019-02-21 13:54:17 +0100

    Add menu to pluggable ui

--- pluggable-ui/ui.js
+++ pluggable-ui/ui.js
@@ -1,17 +1,29 @@
-var fetch_csrf;
+let init_page,
+   treatment_info;
 
 // Make sure that no ajax calls are made before csrf has been retrieved
 $(document).ajaxSend(function (x, y, z) {
    if (z.headers === undefined || z.headers['x-ui-init'] !== true) {
-      if (fetch_csrf().state() !== 'resolved') {
+      if (init_page().state() !== 'resolved') {
          throw 'CSRF has not been retrieved';
       }
    }
 });
 
-fetch_csrf = (function () {
+let populate_menu = function (treatment_info) {
+   let $menu_div = $('#menu');
+   $menu_div.append('<a href="index.html">Dashboard</a>');
+   if (treatment_info['messaging?']) {
+      $menu_div.append('&nbsp;<a href="messages.html">Messages</a>');
+   }
+   $menu_div.append('&nbsp;<a href="modules.html">Modules</a>');
+   $menu_div.append('&nbsp;<a href="privacy-notice.html">Privacy notice</a>');
+   $menu_div.append('&nbsp;<a href="logout.html">Log out</a>');
+};
+
+init_page = (function () {
    let executed = false;
-   let timezone_state = $.Deferred();
+   let tx_info_state = $.Deferred();
    let csrf_state = $.Deferred();
    return function () {
       if (!executed) {
@@ -28,16 +40,22 @@ fetch_csrf = (function () {
             }
          });
 
-         console.log('Fetching timezone');
-         $.ajax('/api/user/timezone-name', {
+         console.log('Fetching treatment info');
+         $.ajax('/api/user/tx/treatment-info', {
             headers: {'x-ui-init': true},
             success: function (data) {
-               console.log('Timezone: ' + data);
-               timezone_state.resolve();
+               console.log('Treatment info fetched');
+               treatment_info = data;
+               populate_menu(treatment_info);
+               tx_info_state.resolve();
             }
          })
 
       }
-      return $.when(csrf_state, timezone_state);
+      return $.when(csrf_state, tx_info_state);
    }
 })();
+
+$(document).ready(function () {
+   init_page();
+});
