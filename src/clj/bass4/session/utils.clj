bb68176e54da9a88a161df896770ea9577ec0a3e
brjann <brjann@gmail.com>
2019-04-16 11:25:39 +0200

    Do not add session keys if session is empty. ATP
    Prevents empty sessions being stored in table.

--- src/clj/bass4/session/utils.clj
+++ src/clj/bass4/session/utils.clj
@@ -3,8 +3,9 @@
 (defn assoc-out-session
   [response session-in merge-map]
   (let [session-out      (:session response)
-        session-deleted? (and (contains? response :session) (empty? session-out))]
-    (if session-deleted?
+        session-deleted? (and (contains? response :session) (empty? session-out))
+        no-session?      (and (empty? session-in) (empty? session-out))]
+    (if (or session-deleted? no-session?)
       response
       (assoc response :session (merge (or session-out
                                           session-in)
