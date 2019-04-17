77197bd1324ffe58746743536073ef8911e96a29
brjann <brjann@gmail.com>
2019-04-17 13:44:11 +0200

    Countdown timer in renew modal

--- resources/public/js/session-status.js
+++ resources/public/js/session-status.js
@@ -3,40 +3,62 @@ var renew_session_password_success, renew_session_click_success;
 $(document).ready(function () {
    var interval_handle,
       first_run = true,
-      timeout_soon = false;
+      timeout_soon = false,
+      $time_to_logout,
+      time_to_logout_handle;
 
    renew_session_password_success = function () {
       timeout_soon = false;
       $(this).find('.alert').hide();
       $(this).find('input').val('');
-      console.log('Password success!');
       $('#renew-session-password-modal').modal('hide');
    };
 
    renew_session_click_success = function () {
       timeout_soon = false;
-      console.log('Click success!');
       $('#renew-session-click-modal').modal('hide');
+   };
+
+   var set_time_to_logout_text = function ($time_to_logout, hard) {
+      var min = Math.floor(hard / 60);
+      var sec = hard % 60;
+      $time_to_logout.text(sprintf(text_time_to_logout, min, sec));
+   };
+
+   update_time_to_logout = function ($time_to_logout, hard) {
+      if (time_to_logout_handle !== undefined) {
+         clearInterval(time_to_logout_handle);
+      }
+      set_time_to_logout_text($time_to_logout, hard);
+      time_to_logout_handle = setInterval(function () {
+         set_time_to_logout_text($time_to_logout, --hard);
+      }, 1000)
    };
 
    var session_checker_success = function (data) {
       console.log(data);
 
       if (first_run && data === null) {
-         console.log('First run and no session info, clearing interval');
          clearInterval(interval_handle);
          return;
       }
       var hard = data.hard,
          re_auth = data['re-auth'];
-      if (hard <= session_timeout_hard_soon && !timeout_soon) {
+
+      if (timeout_soon) {
+         update_time_to_logout($time_to_logout, hard);
+      } else if (hard <= session_timeout_hard_soon) {
          timeout_soon = true;
          console.log('Timeout soon!');
+         var $modal;
          if (re_auth === null) {
-            $('#renew-session-click-modal').modal();
+            $modal = $('#renew-session-click-modal');
          } else {
-            $('#renew-session-password-modal').modal();
+            $modal = $('#renew-session-password-modal');
          }
+         $modal.modal();
+         $time_to_logout = $modal.find('.time-to-logout');
+         update_time_to_logout($time_to_logout, hard);
       }
    };
 
