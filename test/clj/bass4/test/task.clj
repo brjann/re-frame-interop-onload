e32d291c35a7c087a2f0d1fdd69fc098ec61e9ef
brjann <brjann@gmail.com>
2019-06-15 21:20:46 +0200

    Schedule many runs n tasks instead of 6. n = 32. ATP

--- test/clj/bass4/test/task.clj
+++ test/clj/bass4/test/task.clj
@@ -23,30 +23,17 @@
     {}))
 
 (deftest schedule-many
-  (let [c1    (chan 2)
-        c2    (chan 2)
-        c3    (chan 2)
-        c4    (chan 2)
-        c5    (chan 2)
-        c6    (chan 2)
-        task1 (task-fn c1 1)
-        task2 (task-fn c2 2)
-        task3 (task-fn c3 3)
-        task4 (task-fn c4 4)
-        task5 (task-fn c5 5)
-        task6 (task-fn c6 6)]
-    (task-scheduler/schedule! task1 ::task-scheduler/by-millisecond 10)
-    (task-scheduler/schedule! task2 ::task-scheduler/by-millisecond 10)
-    (task-scheduler/schedule! task3 ::task-scheduler/by-millisecond 10)
-    (task-scheduler/schedule! task4 ::task-scheduler/by-millisecond 10)
-    (task-scheduler/schedule! task5 ::task-scheduler/by-millisecond 10)
-    (task-scheduler/schedule! task6 ::task-scheduler/by-millisecond 10)
-    (let [c  (a/map (fn [& more] more) [c1 c2 c3 c4 c5 c6])
+  (let [n  32
+        cs (repeatedly n #(chan 2))
+        fs (for [i (range n)]
+             (task-fn (nth cs i) i))]
+    (doseq [f fs]
+      (task-scheduler/schedule! f ::task-scheduler/by-millisecond 10))
+    (let [c  (a/map (fn [& more] more) cs)
           r1 (alts!! [c (timeout 5000)])
           r2 (alts!! [c (timeout 5000)])]
       (is (= c (second r1)))
       (is (= c (second r2)))
-      (is (= [1 2 3 4 5 6] (first r1)))
-      (is (= [1 2 3 4 5 6] (first r2))))
+      (is (= (range n) (first r1)))
+      (is (= (range n) (first r2))))
     (scheduler/cancel-all!)))
-
