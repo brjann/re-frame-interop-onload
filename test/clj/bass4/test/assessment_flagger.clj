6a6aa796ccaa61273f415dc4b88e9ef98eba1732
brjann <brjann@gmail.com>
2019-11-27 05:51:51 +0100

    Add completed administration to test

--- test/clj/bass4/test/assessment_flagger.clj
+++ test/clj/bass4/test/assessment_flagger.clj
@@ -22,19 +22,26 @@
 (def db-late-flag-group @#'assessment-flagger/db-late-flag-group-administrations)
 
 (deftest db-flag-participant-administration
-  (let [user-id       (user-service/create-user! project-ass1-id)
+  (let [user-id1      (user-service/create-user! project-ass1-id)
+        user-id2      (user-service/create-user! project-ass1-id)
         assessment-id (create-assessment! {"FlagParticipantWhenLate" 1
                                            "DayCountUntilLate"       5})]
     (create-participant-administration!
-      user-id assessment-id 1 {:date (midnight+d -5 *now*)})
+      user-id1 assessment-id 1 {"Date" (midnight+d -5 *now*)})
+    (create-participant-administration!
+      user-id2 assessment-id 1 {"Date"          (midnight+d -5 *now*)
+                                "DateCompleted" 1})
     (is (= 1 (count (db-late-flag-participant *db* *now*))))))
 
 (deftest db-flag-group-administration
   (let [group-id      (create-group!)
-        user-id       (user-service/create-user! project-ass1-id {:group group-id})
+        user-id1      (user-service/create-user! project-ass1-id {:group group-id})
+        user-id2      (user-service/create-user! project-ass1-id {:group group-id})
         assessment-id (create-assessment! {"Scope"                   1
                                            "FlagParticipantWhenLate" 1
                                            "DayCountUntilLate"       5})]
     (create-group-administration!
       group-id assessment-id 1 {:date (midnight+d -5 *now*)})
+    (create-participant-administration!
+      user-id2 assessment-id 1 {"DateCompleted" 1})
     (is (= 1 (count (db-late-flag-group *db* *now*))))))
