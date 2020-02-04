fa2454753a57a36c5c034f282e8d7abeea2bd126
brjann <brjann@gmail.com>
2020-02-04 13:59:20 +0100

    Start rewrite of task scheduler

--- src/clj/bass4/task/scheduler.clj
+++ src/clj/bass4/task/scheduler.clj
@@ -2,7 +2,10 @@
   (:require [clojure.tools.logging :as log]
             [clj-time.core :as t]
             [clojure.repl :as repl]
-            [bass4.task.runner :as task-runner])
+            [bass4.task.runner :as task-runner]
+            [bass4.db-config :as db-config]
+            [bass4.db.core :as db]
+            [bass4.utils :as utils])
   (:import [java.util.concurrent Executors TimeUnit ScheduledExecutorService ScheduledThreadPoolExecutor$ScheduledFutureTask]
            (clojure.lang Var)))
 
@@ -27,9 +30,31 @@
     (.cancel ^ScheduledThreadPoolExecutor$ScheduledFutureTask handle false)
     (swap! tasks #(dissoc % task-name))))
 
+(defn interval-params
+  [scheduling]
+  (case (first scheduling)
+    ::hourly
+    [(- 60 (t/minute (t/now)))
+     60
+     TimeUnit/MINUTES]
+
+    ::by-minute
+    [0
+     (second scheduling)
+     TimeUnit/MINUTES]
+
+    ::by-millisecond
+    [0
+     (second scheduling)
+     TimeUnit/MILLISECONDS]))
+
+(defn task-dbs
+  []
+  (remove #(db-config/db-setting* % [:no-tasks?] false) (keys db/db-connections)))
+
 (defonce task-counter (atom 0))
 
-(defn schedule!
+(defn schedule-db-task!
   [task & scheduling]
   (assert (or (instance? Var task)
               (fn? task)))
@@ -38,28 +63,19 @@
                     (repl/demunge (str task)))]
     (when (contains? @tasks task-name)
       (cancel-task*! task-name))
-    (let [task-id (swap! task-counter inc)]
-      (log/info "Adding task" task-name "with id" task-id)
-      (let [[minutes-left interval time-unit] (case (first scheduling)
-                                                ::hourly
-                                                [(- 60 (t/minute (t/now)))
-                                                 60
-                                                 TimeUnit/MINUTES]
-
-                                                ::by-minute
-                                                [0
-                                                 (second scheduling)
-                                                 TimeUnit/MINUTES]
-
-                                                ::by-millisecond
-                                                [0
-                                                 (second scheduling)
-                                                 TimeUnit/MILLISECONDS])
-            handle (.scheduleAtFixedRate schedule-pool
-                                         (bound-fn*
-                                           #(task-runner/run-task-for-dbs! task task-name task-id))
-                                         (long minutes-left)
-                                         interval
-                                         time-unit)]
-        (swap! tasks #(assoc % task-name [task-id handle]))
-        task-id))))
+    (doseq [db-name (task-dbs)]
+      (let [task-id (swap! task-counter inc)]
+        (let [[minutes-left interval time-unit] (interval-params scheduling)
+              handle (.scheduleAtFixedRate schedule-pool
+                                           (bound-fn*
+                                             (fn []
+                                               (let [db        @(get db/db-connections db-name)
+                                                     db-config (get db-config/local-configs db-name)]
+                                                 (log/debug "Running task " task-name "with id" task-id "for" db-name)
+                                                 (task-runner/run-db-task! db (t/now) db-name db-config task task-name task-id))))
+                                           (long minutes-left)
+                                           interval
+                                           time-unit)]
+          (log/info "Adding task" task-name "with id" task-id "for" db-name "")
+          (utils/swap-key! tasks task-name #(conj % [handle db-name task-id]) [[handle db-name task-id]])
+          task-id)))))
