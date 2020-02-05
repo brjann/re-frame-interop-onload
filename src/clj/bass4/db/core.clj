5defa24fcb5485ea68c84c21c637d13aa7f41882
brjann <brjann@gmail.com>
2020-02-05 10:29:10 +0100

    Connections are made from client-db but locals not yet removed

--- src/clj/bass4/db/core.clj
+++ src/clj/bass4/db/core.clj
@@ -1,14 +1,15 @@
 (ns bass4.db.core
   (:require
     [ring.util.codec :refer [url-encode]]
-    [bass4.utils :refer [map-map filter-map time+ val-to-bool]]
+    [bass4.utils :as utils :refer [map-map filter-map time+ val-to-bool]]
     [conman.core :as conman]
     [mount.core :refer [defstate]]
     [bass4.db-config :as db-config]
     [clojure.tools.logging :as log]
     ;; clj-time.jdbc registers protocol extensions so you don’t have to use clj-time.coerce yourself to coerce to and from SQL timestamps.
     [clj-time.jdbc]
-    [bass4.config :as config]))
+    [bass4.config :as config]
+    [clojure.java.jdbc :as jdbc]))
 
 ;----------------
 ; SETUP DB STATE
@@ -52,17 +53,18 @@
     (:db-user local-config)
     (:db-password local-config)))
 
-
-#_(defstate metrics-reg
-    :start (let [metrics-reg (metrics/new-registry)
-                 CR          (csv/reporter
-                               metrics-reg
-                               (str (config/env :bass-path) "/projects/system/bass4-db-log") {:locale (Locale/US)})
-                 report-freq (env :metrics-report-freq 60)]
-             (csv/start CR report-freq)
-             metrics-reg)
-    :stop (do))
-
+(defn jdbc-map
+  [local-config]
+  {:jdbc-url            (str (db-url local-config (config/env :database-port))
+                             "&serverTimezone=UTC"
+                             "&jdbcCompliantTruncation=false"
+                             "&useUnicode=true"
+                             "&characterEncoding=utf8"
+                             "&useSSL=false")
+   :pool-name           (:name local-config)
+   ;:metric-registry   metrics-reg
+   :maximum-pool-size   5
+   :connection-init-sql "SET time_zone = '+00:00';"})
 
 (defn db-connect!
   ;; NOTE: MySQL variables cannot be executed on conn
@@ -70,21 +72,11 @@
   ;; And :connection-init-sql only accepts one command.
   ;; Proper SQL mode (e.g. MYSQL40) must therefore be set in my.cnf
   [local-config]
-  (let [url (db-url local-config (config/env :database-port))]
-    (delay
-      (log/info (str "Attaching " (:name local-config)))
-      (let [conn (conman/connect! {:jdbc-url            (str url
-                                                             "&serverTimezone=UTC"
-                                                             "&jdbcCompliantTruncation=false"
-                                                             "&useUnicode=true"
-                                                             "&characterEncoding=utf8"
-                                                             "&useSSL=false")
-                                   :pool-name           (:name local-config)
-                                   ;:metric-registry   metrics-reg
-                                   :maximum-pool-size   5
-                                   :connection-init-sql "SET time_zone = '+00:00';"})]
-        (log/info (str (:name local-config) " attached"))
-        conn))))
+  (delay
+    (log/info (str "Attaching " (:name local-config)))
+    (let [conn (conman/connect! (jdbc-map local-config))]
+      (log/info (str (:name local-config) " attached"))
+      conn)))
 
 (defn db-disconnect!
   [db-conn]
@@ -92,25 +84,34 @@
     (log/info (str "Detaching db"))
     (conman/disconnect! @db-conn)))
 
+(defstate db-common
+  :start @(db-connect! db-config/common-config)
+  :stop (do (log/info "Detaching common")
+            (conman/disconnect! db-common)))
+
+(defn- load-client-db-configs
+  [db-common]
+  (jdbc/query db-common ["SELECT * from client_dbs"]))
+
+(defstate client-db-configs
+  :start (let [configs (load-client-db-configs db-common)]
+           (->> configs
+                (map #(assoc % :name (:id-name %)))
+                (map (juxt (comp keyword :id-name) identity))
+                (into {}))))
 
 ;; Bind queries to *db* dynamic variable which is bound
 ;; to each clients database before executing queries
 (def ^:dynamic *db* nil)
 
 (defstate db-connections
-  :start (let [x (map-map db-connect!
-                          db-config/local-configs)]
+  :start (let [x (utils/map-map db-connect! client-db-configs)]
            (when (config/env :dev)
              (log/info "Setting *db* to dev database")
              (def ^:dynamic *db* @(get x (config/env :dev-db))))
            x)
   :stop (map-map db-disconnect!
                  db-connections))
-
-(defstate db-common
-  :start @(db-connect! db-config/common-config)
-  :stop (do (log/info "Detaching common")
-            (conman/disconnect! db-common)))
 
 (conman/bind-connection *db*
                         "sql/bass.sql"
@@ -133,3 +134,14 @@
 
 ;; clj-time.jdbc registers protocol extensions,
 ;; so you don’t have to use clj-time.coerce yourself to coerce to and from SQL timestamps.
+
+
+#_(defstate metrics-reg
+    :start (let [metrics-reg (metrics/new-registry)
+                 CR          (csv/reporter
+                               metrics-reg
+                               (str (config/env :bass-path) "/projects/system/bass4-db-log") {:locale (Locale/US)})
+                 report-freq (env :metrics-report-freq 60)]
+             (csv/start CR report-freq)
+             metrics-reg)
+    :stop (do))
