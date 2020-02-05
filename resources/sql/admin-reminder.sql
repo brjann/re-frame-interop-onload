8134d3cc88d1aee2b66373183ebb0c312f0e0592
brjann <brjann@gmail.com>
2020-02-05 15:42:42 +0100

    Admin reminder SQL queries

---
+++ resources/sql/admin-reminder.sql
@@ -0,0 +1,57 @@
+-- :name admin-reminder-unread-messages :? :*
+-- :doc
+SELECT
+  cm.ParentId AS `user-id`,
+  min(SendTime) AS `send-time`
+FROM c_message AS cm
+JOIN links_c_message AS pl
+  ON pl.PropertyName = 'Sender' AND cm.objectid = pl.LinkerId AND cm.parentid = pl.linkeeid
+WHERE
+  readtime = 0
+  AND draft = 0
+  AND cm.ParentProperty = 'Messages'
+  AND SendTime > :time-limit
+GROUP BY cm.ParentId;
+
+-- :name admin-reminder-unread-homework :? :*
+-- :doc
+SELECT
+  cta.`ParentId` AS `user-id`,
+  min(cdh.`Time`) AS `send-time`
+FROM content_data_homework AS cdh
+JOIN c_treatmentaccess AS cta
+  ON cdh.Id IN(SELECT Max(Id)
+          FROM content_data_homework
+          GROUP BY TreatmentAccessId, ModuleId)
+  AND (cdh.InspectionTime = 0)
+  AND cdh.TreatmentAccessId = cta.ObjectId
+  AND cdh.`Time` > :time-limit
+JOIN links_c_treatmentaccess AS lca
+    ON cta.ObjectId = lca.LinkerId AND lca.PropertyName = "Treatment"
+JOIN c_treatment AS ct
+  ON lca.LinkeeId = ct.ObjectId
+  AND ct.UseHomeworkInspection = 1
+GROUP BY cdh.ModuleId, `user-id`;
+
+-- :name admin-reminder-open-flags :? :*
+-- :doc
+SELECT
+	ParentId AS `user-id`,
+    min(Created) AS `send-time`
+FROM c_flag
+WHERE
+`ClosedAt` = 0
+AND Created > :time-limit
+GROUP BY ParentId;
+
+-- :name admin-reminder-lost-password-flags :? :*
+-- :doc
+SELECT
+	ParentId AS `user-id`,
+    min(Created) AS `send-time`
+FROM c_flag
+WHERE
+	`ClosedAt` = 0
+    AND `Issuer` = "lost-password"
+    AND Created > :time-limit
+GROUP BY ParentId;
