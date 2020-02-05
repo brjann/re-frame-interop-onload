0188bf84d7224033eb3921cd38a70d567e0910eb
brjann <brjann@gmail.com>
2020-02-05 21:38:44 +0100

    DB calls and test for unread messages

--- src/clj/bass4/task/admin_reminder.clj
+++ src/clj/bass4/task/admin_reminder.clj
@@ -1,1 +1,19 @@
-(ns bass4.task.admin-reminder)
+(ns bass4.task.admin-reminder
+  (:require [bass4.utils :as utils]
+            [bass4.db.core :as db]))
+
+(defn db-lost-password-flags
+  [time-limit]
+  (db/admin-reminder-lost-password-flags {:time-limit (utils/to-unix time-limit)}))
+
+(defn db-open-flags
+  [time-limit]
+  (db/admin-reminder-open-flags {:time-limit (utils/to-unix time-limit)}))
+
+(defn db-unread-messages
+  [time-limit]
+  (db/admin-reminder-unread-messages {:time-limit (utils/to-unix time-limit)}))
+
+(defn db-unread-homework
+  [time-limit]
+  (db/admin-reminder-unread-homework {:time-limit (utils/to-unix time-limit)}))
