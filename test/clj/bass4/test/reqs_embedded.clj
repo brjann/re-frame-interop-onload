ffd0ebeeebba263d6eb3eb5c788ca853e1dd4226
brjann <brjann@gmail.com>
2020-02-14 11:18:49 +0100

    Embedded session tests updated to use uid-for-data

--- test/clj/bass4/test/reqs_embedded.clj
+++ test/clj/bass4/test/reqs_embedded.clj
@@ -49,9 +49,9 @@
   (let [php-session-id (get-php-session-id)
         now            (utils/to-unix (t/now))]
     (jdbc/insert! db/*db* "sessions" {"SessId" php-session-id "UserId" 110 "LastActivity" now "SessionStart" now})
-    (with-redefs [php-interop/read-session-file (constantly {:user-id 110 :path "instrument/1647" :php-session-id php-session-id})]
+    (let [uid (php-interop/uid-for-data! {:user-id 110 :path "instrument/1647" :php-session-id php-session-id})]
       (-> *s*
-          (visit "/embedded/create-session?uid=8&redirect=https://www.dn.se")
+          (visit (str "/embedded/create-session?uid=" uid))
           (visit "/embedded/instrument/1647")
           (has (status? 200))
           (visit "/embedded/instrument/1647" :request-method :post :params {})
@@ -67,14 +67,14 @@
 
 
 (deftest request-wrong-instrument
-  (with-redefs [php-interop/read-session-file (constantly {:user-id 110 :path "instrument/" :php-session-id "xxx"})
-                php-interop/get-php-session   (constantly {:user-id 110 :last-activity (utils/to-unix (t/now))})]
-    (-> *s*
-        (visit "/embedded/create-session?uid=8&redirect=https://www.dn.se")
-        (visit "/embedded/instrument/hell-is-here")
-        (has (status? 400))
-        (visit "/embedded/instrument/666")
-        (has (status? 404)))))
+  (let [uid (php-interop/uid-for-data! {:user-id 110 :path "instrument/" :php-session-id "xxx"})]
+    (with-redefs [php-interop/get-php-session (constantly {:user-id 110 :last-activity (utils/to-unix (t/now))})]
+      (-> *s*
+          (visit (str "/embedded/create-session?uid=" uid))
+          (visit "/embedded/instrument/hell-is-here")
+          (has (status? 400))
+          (visit "/embedded/instrument/666")
+          (has (status? 404))))))
 
 (deftest request-not-embedded
   (-> *s*
@@ -82,94 +82,97 @@
       (has (status? 403))))
 
 (deftest embedded-render
-  (with-redefs [php-interop/read-session-file (constantly {:user-id 110 :path "iframe/render" :php-session-id "xxx"})
-                php-interop/get-php-session   (constantly {:user-id 110 :last-activity (utils/to-unix (t/now))})]
-    (-> *s*
-        (visit "/embedded/iframe/render")
-        (has (status? 403))
-        (visit "/embedded/iframe/render?uid=x" :request-method :post :params {:text "Hejsan"})
-        (has (status? 200))
-        (has (some-text? "Hejsan")))))
+  (let [uid (php-interop/uid-for-data! {:user-id 110 :path "iframe/render" :php-session-id "xxx"})]
+    (with-redefs [php-interop/get-php-session (constantly {:user-id 110 :last-activity (utils/to-unix (t/now))})]
+      (-> *s*
+          (visit "/embedded/iframe/render")
+          (has (status? 403))
+          (visit (str "/embedded/create-session?uid=" uid))
+          (visit "/embedded/iframe/render" :request-method :post :params {:text "Hejsan"})
+          (has (status? 200))
+          (has (some-text? "Hejsan"))))))
 
 (deftest session-timeout
   (fix-time
     (let [php-session-id   (get-php-session-id)
+          uid              (php-interop/uid-for-data! {:user-id 110 :path "instrument/1647" :php-session-id php-session-id})
           now              (utils/to-unix (t/now))
           timeouts         (php-interop/get-staff-timeouts)
           re-auth-timeout  (:re-auth-timeout timeouts)
           absolute-timeout (:absolute-timeout timeouts)]
       (jdbc/insert! db/*db* "sessions" {"SessId" php-session-id "UserId" 110 "LastActivity" now "SessionStart" now})
-      (with-redefs [php-interop/read-session-file (constantly {:user-id 110 :path "instrument/1647" :php-session-id php-session-id})]
-        (-> *s*
-            (visit "/embedded/create-session?uid=8")
-            (visit "/embedded/instrument/1647")
-            (has (status? 200))
-            ;; Advance time to re-auth-timeout
-            (advance-time-s! re-auth-timeout)
-            (visit "/embedded/instrument/1647")
-            (follow-redirect)
-            (has (some-text? "Timeout"))
-            ;; Fake re-auth
-            (pass-by (php-interop/update-php-session-last-activity! php-session-id (utils/to-unix (t/now))))
-            (visit "/embedded/instrument/1647")
-            (has (status? 200))
-            ;; Advance time to re-auth-timeout in two steps
-            (advance-time-s! (dec re-auth-timeout))
-            (advance-time-s! 1)
-            (visit "/embedded/instrument/1647")
-            (has (status? 302))
-            (pass-by (php-interop/update-php-session-last-activity! php-session-id (utils/to-unix (t/now))))
-            ;; Advance time to almost re-auth-timeout
-            (advance-time-s! (dec re-auth-timeout))
-            ;; Reload page (updating last activity)
-            (visit "/embedded/instrument/1647")
-            (has (status? 200))
-            ;; Advance time to full re-auth-timeout
-            (advance-time-s! 1)
-            ;; Still logged in because of previous page reload
-            (visit "/embedded/instrument/1647")
-            (has (status? 200))
-            ;; Advance time to absolute-timeout
-            (advance-time-s! absolute-timeout)
-            (visit "/embedded/instrument/1647")
-            (follow-redirect)
-            (has (some-text? "No session"))
-            ;; Reload page (updating last activity)
-            (pass-by (php-interop/update-php-session-last-activity! php-session-id (utils/to-unix (t/now))))
-            (visit "/embedded/instrument/1647")
-            ;; Access error - session was destroyed
-            (has (status? 403)))))))
+      (-> *s*
+          (visit (str "/embedded/create-session?uid=" uid))
+          (visit "/embedded/instrument/1647")
+          (has (status? 200))
+          ;; Advance time to re-auth-timeout
+          (advance-time-s! re-auth-timeout)
+          (visit "/embedded/instrument/1647")
+          (follow-redirect)
+          (has (some-text? "Timeout"))
+          ;; Fake re-auth
+          (pass-by (php-interop/update-php-session-last-activity! php-session-id (utils/to-unix (t/now))))
+          (visit "/embedded/instrument/1647")
+          (has (status? 200))
+          ;; Advance time to re-auth-timeout in two steps
+          (advance-time-s! (dec re-auth-timeout))
+          (advance-time-s! 1)
+          (visit "/embedded/instrument/1647")
+          (has (status? 302))
+          (pass-by (php-interop/update-php-session-last-activity! php-session-id (utils/to-unix (t/now))))
+          ;; Advance time to almost re-auth-timeout
+          (advance-time-s! (dec re-auth-timeout))
+          ;; Reload page (updating last activity)
+          (visit "/embedded/instrument/1647")
+          (has (status? 200))
+          ;; Advance time to full re-auth-timeout
+          (advance-time-s! 1)
+          ;; Still logged in because of previous page reload
+          (visit "/embedded/instrument/1647")
+          (has (status? 200))
+          ;; Advance time to absolute-timeout
+          (advance-time-s! absolute-timeout)
+          (visit "/embedded/instrument/1647")
+          (follow-redirect)
+          (has (some-text? "No session"))
+          ;; Reload page (updating last activity)
+          (pass-by (php-interop/update-php-session-last-activity! php-session-id (utils/to-unix (t/now))))
+          (visit "/embedded/instrument/1647")
+          ;; Access error - session was destroyed
+          (has (status? 403))))))
 
 (deftest path-merge
   (fix-time
     (let [php-session-id-1 (get-php-session-id)
           php-session-id-2 (get-php-session-id)
           now              (utils/to-unix (t/now))
+          uid1             (php-interop/uid-for-data! {:user-id 110 :path "instrument/1647" :php-session-id php-session-id-1})
+          uid2             (php-interop/uid-for-data! {:user-id 110 :path "instrument/286" :php-session-id php-session-id-1})
+          uid3             (php-interop/uid-for-data! {:user-id 110 :path "instrument/286" :php-session-id php-session-id-2})
           session-files    (atom [{:user-id 110 :path "instrument/286" :php-session-id php-session-id-2}
                                   {:user-id 110 :path "instrument/286" :php-session-id php-session-id-1}
                                   {:user-id 110 :path "instrument/1647" :php-session-id php-session-id-1}])]
       (jdbc/insert! db/*db* "sessions" {"SessId" php-session-id-1 "UserId" 110 "LastActivity" now "SessionStart" now})
       (jdbc/insert! db/*db* "sessions" {"SessId" php-session-id-2 "UserId" 110 "LastActivity" now "SessionStart" now})
-      (with-redefs [php-interop/read-session-file (fn [_] (utils/queue-pop! session-files))]
-        (-> *s*
-            ;; First session file gives access to 1647
-            (visit "/embedded/create-session?uid=8")
-            (visit "/embedded/instrument/1647")
-            (has (status? 200))
-            ;; No access to 286 yet
-            (visit "/embedded/instrument/286")
-            (has (status? 403))
-            ;; First session file gives access to 286
-            (visit "/embedded/create-session?uid=8")
-            (visit "/embedded/instrument/286")
-            (has (status? 200))
-            ;; Access to 1647 has been kept
-            (visit "/embedded/instrument/1647")
-            (has (status? 200))
-            ;; Third session file changes session id and gives access to 286
-            (visit "/embedded/create-session?uid=8")
-            (visit "/embedded/instrument/286")
-            (has (status? 200))
-            ;; No access to 1647 because of changed session id
-            (visit "/embedded/instrument/1647")
-            (has (status? 403)))))))
+      (-> *s*
+          ;; First session file gives access to 1647
+          (visit (str "/embedded/create-session?uid=" uid1))
+          (visit "/embedded/instrument/1647")
+          (has (status? 200))
+          ;; No access to 286 yet
+          (visit "/embedded/instrument/286")
+          (has (status? 403))
+          ;; First session file gives access to 286
+          (visit (str "/embedded/create-session?uid=" uid2))
+          (visit "/embedded/instrument/286")
+          (has (status? 200))
+          ;; Access to 1647 has been kept
+          (visit "/embedded/instrument/1647")
+          (has (status? 200))
+          ;; Third session file changes session id and gives access to 286
+          (visit (str "/embedded/create-session?uid=" uid3))
+          (visit "/embedded/instrument/286")
+          (has (status? 200))
+          ;; No access to 1647 because of changed session id
+          (visit "/embedded/instrument/1647")
+          (has (status? 403))))))
