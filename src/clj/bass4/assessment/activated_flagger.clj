99e5f90a093523df8b43dba68be8ea74cd2d79e9
brjann <brjann@gmail.com>
2020-02-21 10:40:04 +0100

    Move activated flag db calls into assessment-db ns. ATP

--- src/clj/bass4/assessment/activated_flagger.clj
+++ src/clj/bass4/assessment/activated_flagger.clj
@@ -1,39 +1,14 @@
 (ns bass4.assessment.activated-flagger
   (:require [clj-time.core :as t]
             [clojure.core.async :refer [put!]]
-            [bass4.db.core :as db]
             [bass4.assessment.reminder :as assessment-reminder]
-            [bass4.services.bass :as bass]
+            [bass4.assessment.db :as assessment-db]
             [bass4.assessment.create-missing :as missing]
             [clj-time.format :as tf]
-            [bass4.db.orm-classes :as orm]
-            [bass4.clients.time :as client-time]))
+            [bass4.db.orm-classes :as orm]))
 
 (def flag-issuer "tActivatedAdministrationsFlagger")
 (def oldest-allowed 100)
-
-(defn date-intervals
-  [now tz]
-  (let [midnight (client-time/local-midnight now tz)]
-    [(-> midnight
-         (t/minus (t/days oldest-allowed)))
-     (-> midnight
-         (t/plus (t/days 1))
-         (t/minus (t/seconds 1)))]))
-
-(defn- db-participant-administrations
-  [db now tz]
-  (let [[date-min date-max] (date-intervals now tz)]
-    (db/get-flagging-activated-participant-administrations db {:date-max date-max
-                                                               :date-min date-min
-                                                               :issuer   flag-issuer})))
-
-(defn- db-group-administrations
-  [db now tz]
-  (let [[date-min date-max] (date-intervals now tz)]
-    (db/get-flagging-activated-group-administrations db {:date-max date-max
-                                                         :date-min date-min
-                                                         :issuer   flag-issuer})))
 
 (defn flag-text
   [tz assessment]
@@ -73,8 +48,18 @@
    :assessment-id 653636,
    :assessment-index 1,}"
   [db now tz]
-  (let [participant-administrations (db-participant-administrations db now tz)
-        group-administration        (db-group-administrations db now tz)]
+  (let [participant-administrations (assessment-db/activated-flag-participant-administrations
+                                      db
+                                      now
+                                      tz
+                                      flag-issuer
+                                      oldest-allowed)
+        group-administration        (assessment-db/activated-flag-group-administrations
+                                      db
+                                      now
+                                      tz
+                                      flag-issuer
+                                      oldest-allowed)]
     (concat participant-administrations
             group-administration)))
 
---
+++ src/clj/bass4/assessment/db.clj
@@ -0,0 +1,30 @@
+(ns bass4.assessment.db
+  (:require [bass4.db.core :as db]
+            [bass4.clients.time :as client-time]
+            [clj-time.core :as t]
+            [clojure.tools.logging :as log]))
+
+(defn date-intervals
+  [now tz oldest-allowed]
+  (let [midnight (client-time/local-midnight now tz)]
+    [(-> midnight
+         (t/minus (t/days oldest-allowed)))
+     (-> midnight
+         (t/plus (t/days 1))
+         (t/minus (t/seconds 1)))]))
+
+(defn activated-flag-group-administrations
+  [db now tz flag-issuer oldest-allowed]
+  (let [[date-min date-max] (date-intervals now tz oldest-allowed)]
+    (db/get-flagging-activated-group-administrations db {:date-max date-max
+                                                         :date-min date-min
+                                                         :issuer   flag-issuer})))
+
+(defn activated-flag-participant-administrations
+  [db now tz flag-issuer oldest-allowed]
+  (log/debug flag-issuer)
+  (log/debug oldest-allowed)
+  (let [[date-min date-max] (date-intervals now tz oldest-allowed)]
+    (db/get-flagging-activated-participant-administrations db {:date-max date-max
+                                                               :date-min date-min
+                                                               :issuer   flag-issuer})))
