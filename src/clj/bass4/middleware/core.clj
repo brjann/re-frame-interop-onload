be0957a2505d11d40d002196e82db05b6bd63103
brjann <brjann@gmail.com>
2020-05-05 21:44:54 +0200

    Add ki.se as trusted iframe src

--- src/clj/bass4/middleware/core.clj
+++ src/clj/bass4/middleware/core.clj
@@ -68,7 +68,7 @@
     (update-in response
                [:headers] #(merge %1
                                   {"Content-Security-Policy"   (str "default-src 'self';"
-                                                                    "frame-src https://*.vimeo.com 'self';"
+                                                                    "frame-src https://*.vimeo.com https://*.ki.se 'self';"
                                                                     "script-src 'unsafe-inline' 'unsafe-eval' 'self';"
                                                                     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;"
                                                                     "img-src * data:;"
