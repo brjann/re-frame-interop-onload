91939f5cc978fb49e715a423c8569bb8c255151b
brjann <brjann@gmail.com>
2020-05-14 20:08:08 +0200

    Create uid for password and store in passwords table. Delete old password entries

--- src/clj/bass4/password/get_services.clj
+++ src/clj/bass4/password/get_services.clj
@@ -1,6 +1,18 @@
 (ns bass4.password.get-services
-  (:require [bass4.passwords :as passwords]))
+  (:require [bass4.passwords :as passwords]
+            [clojure.java.jdbc :as jdbc]
+            [bass4.db.core :as db]
+            [bass4.now :as now]
+            [clj-time.core :as t]))
 
 (defn gen-uid
   []
   (passwords/letters-digits 13 passwords/url-safe-chars))
+
+(defn create-uid!
+  [password]
+  (let [uid         (gen-uid)
+        valid-until (t/plus (now/now) (t/days 2))]
+    (jdbc/execute! db/db-common ["INSERT INTO passwords (`uid`, `password`, `valid-until`) VALUES (?,?,?)"
+                                 uid, password valid-until])
+    uid))
